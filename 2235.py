f642b87cb3b8f8b0239f6f57862f1a1c282c51f5
Angel Piña <inbox.angelpinha@gmail.com>
2023-04-05 22:23:03 -0400

    Add Two Integers

---
+++ 2235.py
@@ -0,0 +1,26 @@
+# 2235. Add Two Integers
+# https://leetcode.com/problems/add-two-integers/
+# Solution: Angel Piña
+
+import pytest
+
+
+class Solution:
+    def sum(self, num1: int, num2: int) -> int:
+        return num1 + num2
+
+# Tests for the given solution
+class Tests:
+    @pytest.mark.parametrize(
+        "i1, i2, output",
+        [
+            (12, 5, 17),
+            (-10, 4, -6),
+        ],
+    )
+    def test_sum(self, i1, i2, output):
+        assert Solution().sum(i1, i2) == output
+
+
+if __name__ == "__main__":
+    pytest.main(["-v", __file__])
